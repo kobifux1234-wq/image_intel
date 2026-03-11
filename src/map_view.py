64b2efe6741fa3fed4ec98d3f67d8a169f038eda
Netanel Pardes <sendi8475@gmail.com>
2026-03-11 14:45:20 +0200

    File order changes

--- src/map_view.py
+++ src/map_view.py
@@ -1,5 +1,6 @@
 import folium
 import extractor
+from pathlib import Path
 
 FOLIUM_SUPPORTED_COLORS = [
     'red', 'blue', 'green', 'purple', 'orange', 'darkred',
@@ -8,12 +9,14 @@ FOLIUM_SUPPORTED_COLORS = [
     'gray', 'black', 'lightgray'
 ]
 
+
 def sort_by_time(arr):
-    return sorted(arr,key= lambda x: (x["datetime"] is None,x["datetime"]))
+    return sorted(arr, key=lambda x: (x["datetime"] is None, x["datetime"]))
+
 
 def color_to_model(arr):
-    colors_models={}
-    for i,img in enumerate(arr):
+    colors_models = {}
+    for i, img in enumerate(arr):
         model = img.get('camera_model', 'Unknown')
         if model not in colors_models:
             colors_models[model] = FOLIUM_SUPPORTED_COLORS[i % len(FOLIUM_SUPPORTED_COLORS)]
@@ -21,57 +24,73 @@ def color_to_model(arr):
 
 
 def create_map(images_data):
-    sorted_image_data=sort_by_time(images_data)
-    colors_model=color_to_model(images_data)
-    m=folium.Map()
-    locations=[]
-    last_datetime=None
+    sorted_image_data = sort_by_time(images_data)
+    colors_model = color_to_model(images_data)
+    m = folium.Map()
+    locations = []
+    last_datetime = None
+
     for img in sorted_image_data:
         if img.get('latitude') is None or img.get('longitude') is None:
             continue
+
         if img.get("has_gps"):
-            img_path=f'../images/nigga/{img['filename']}'
-            html_content= f"""
-            <img src='{img_path}' width="200"><br>
+            img_path = f"../images/nigga/{img['filename']}"
+            camera_model = img["camera_model"].replace('\x00', "")
+
+            html_content = f"""
+            <img src="{img_path}" width="200"><br>
             <b>Name:</b> {img['filename']}<br>
             <b>Time:</b> {img['datetime']}<br>
-            <b>Model</b> {img["camera_model"].replace('\x00',"")}<br>
-            <b>Coordinates</b> {img["latitude"]}, {img["longitude"]}
+            <b>Model:</b> {camera_model}<br>
+            <b>Coordinates:</b> {img["latitude"]}, {img["longitude"]}
             """
 
             loc = [img["latitude"], img["longitude"]]
             locations.append(loc)
-            popup = folium.Popup(html_content)
-            icon_file=folium.Icon(color= colors_model[img['camera_model']])
 
-            folium.Marker(location=loc, popup=popup,icon=icon_file,tooltip=f'{img['camera_model'].replace('\x00',"")} {img['datetime']}').add_to(m)
+            popup = folium.Popup(html_content)
+            icon_file = folium.Icon(color=colors_model[img['camera_model']])
+
+            folium.Marker(
+                location=loc,
+                popup=popup,
+                icon=icon_file,
+                tooltip=f"{camera_model} {img['datetime']}"
+            ).add_to(m)
 
             if len(locations) > 1:
                 folium.PolyLine(
-                    locations=[locations[-2],locations[-1]],
+                    locations=[locations[-2], locations[-1]],
                     color="blue",
                     weight=5,
                     opacity=0.6,
-                    tooltip= f'{last_datetime} --->  {img['datetime']}',
+                    tooltip=f"{last_datetime} ---> {img['datetime']}",
                     sticky=True
-                    #dash_array='5, 10'  # אופציונלי: הופך את הקו למקווקו
                 ).add_to(m)
-            last_datetime=img['datetime']
 
+            last_datetime = img['datetime']
 
     if locations:
-        m.fit_bounds(locations,max_zoom=12)
+        m.fit_bounds(locations, max_zoom=12)
+
     header = '<meta http-equiv="content-type" content="text/html; charset=UTF-8" />'
     return header + m._repr_html_()
 
 
-
-
-
-
 if __name__ == "__main__":
-    data=extractor.extract_all('../images/nigga')
+    base_dir = Path(__file__).resolve().parent.parent
+    images_path = base_dir / "images" / "nigga"
+
+    data = extractor.extract_all(str(images_path))
     html = create_map(data)
-    with open("test_map.html", "w", encoding="utf-8") as f:
+
+    output_dir = base_dir / "output"
+    output_dir.mkdir(exist_ok=True)
+
+    output_file = output_dir / "test_map.html"
+
+    with open(output_file, "w", encoding="utf-8") as f:
         f.write(html)
-    print("Map saved to test_map.html")
+
+    print(f"Map saved to {output_file}")
